6c036d68a63d65d55c0dbb34924a666b582ff693
Richard Doe <richard.doe@rwdit.net>
2017-12-09 12:21:35 +0000

    Send permitted parameters to OAI-PMH provider
    For <request/> attributes

--- app/controllers/oai_controller.rb
+++ app/controllers/oai_controller.rb
@@ -2,10 +2,16 @@
 
 class OAIController < ApplicationController
   def index
-    options = params.except(:controller, :action)
     provider = Europeana::Stories::OAI::Provider.new
+    options = params.permit(*oai_pmh_request_arguments).to_hash
     # TODO: this fails if no ORE::Aggregation documents exist
     response =  provider.process_request(options)
     render xml: response
+  end
+
+  protected
+
+  def oai_pmh_request_arguments
+    %i(verb identifier metadataPrefix from until set resumptionToken)
   end
 end
