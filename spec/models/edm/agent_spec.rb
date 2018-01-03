72c53498715f9048a7658fb3be1686fd61ef7b00
Richard Doe <richard.doe@rwdit.net>
2018-01-03 14:26:38 +0000

    Spec CampaignValidatableModel concern

--- spec/models/edm/agent_spec.rb
+++ spec/models/edm/agent_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe EDM::Agent do
   describe 'modules' do
     subject { described_class }
     it { is_expected.to include(Mongoid::Document) }
+    it { is_expected.to include(CampaignValidatableModel) }
     it { is_expected.to include(RDFModel) }
     it { is_expected.to include(RemoveBlankAttributes) }
   end
