e1517c9409f4387ddef506be7dfa48019a23d678
Richard Doe <richard.doe@rwdit.net>
2018-01-04 09:47:19 +0000

    UNESCO vocab: check prefLabel and altLabel for match

--- app/controllers/vocabularies/unesco_controller.rb
+++ app/controllers/vocabularies/unesco_controller.rb
@@ -8,13 +8,27 @@ module Vocabularies
                      },
                      query: :query,
                      results: 'results',
-                     text: 'prefLabel',
+                     text: :index_result_text,
                      value: 'uri'
 
     protected
 
     def index_query
       "#{params[:q]}*"
+    end
+
+    # matches may be in either prefLabel or altLabel
+    def index_result_text(result)
+      query = params[:q]
+      regex = /\A#{query}/i
+
+      if !!(result['prefLabel'] =~ regex)
+        result['prefLabel']
+      elsif !!(result['altLabel'] =~ regex)
+        result['altLabel']
+      else
+        result['prefLabel']
+      end
     end
   end
 end
