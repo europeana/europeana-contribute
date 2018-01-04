64dfca0894707042d77ac72c7a172d306f1c2f5b
Richard Doe <richard.doe@rwdit.net>
2018-01-04 10:19:59 +0000

    Scan prefLabel and altLabel for Europeana place entity match

--- app/controllers/vocabularies/europeana/places_controller.rb
+++ app/controllers/vocabularies/europeana/places_controller.rb
@@ -10,13 +10,53 @@ module Vocabularies
                        },
                        query: :text,
                        results: 'items',
-                       text: lambda { |result|
-                         result['prefLabel'][I18n.locale.to_s] ||
-                           result['prefLabel'][I18n.default_locale.to_s] ||
-                           result['prefLabel'][''] ||
-                           result['prefLabel'].values.first
-                       },
+                       text: :index_result_text,
                        value: 'id'
+
+      protected
+
+      def index_result_text(result)
+        candidates = index_result_text_candidates(result)
+
+        index_result_text_matching_query(candidates) ||
+          index_result_text_present(candidates)
+      end
+
+      def index_result_text_candidates(result)
+        result['altLabel'] ||= {}
+        [
+          result['prefLabel'][I18n.locale.to_s],
+          result['altLabel'][I18n.locale.to_s],
+          result['prefLabel'][I18n.default_locale.to_s],
+          result['altLabel'][I18n.default_locale.to_s],
+          result['prefLabel'][''],
+          result['altLabel'][''],
+          result['prefLabel'].values,
+          result['altLabel'].values
+        ]
+      end
+
+      # Find and return the first candidate matching the regex
+      def index_result_text_matching_query(candidates)
+        regex = /\b#{params[:q]}/i
+
+        candidates.each do |candidate|
+          match = [candidate].flatten.detect { |value| !!(value =~ regex) }
+          return match unless match.nil?
+        end
+
+        nil
+      end
+
+      # Find and the first non-blank candidate
+      def index_result_text_present(candidates)
+        candidates.each do |candidate|
+          present = [candidate].flatten.detect { |value| value.present? }
+          return present unless present.nil?
+        end
+
+        nil
+      end
     end
   end
 end
