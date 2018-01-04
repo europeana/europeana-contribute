c605b7a7138b9f5342e97d852444dc1ac17e2600
Richard Doe <richard.doe@rwdit.net>
2018-01-04 14:14:37 +0000

    Configure RailsAdmin for Devise user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,4 +39,20 @@ class User
   # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
+
+  rails_admin do
+    object_label_method { :email }
+
+    list do
+      field :email
+      field :current_sign_in_at
+      field :current_sign_in_ip
+    end
+
+    edit do
+      field :email
+      field :password
+      field :password_confirmation
+    end
+  end
 end
