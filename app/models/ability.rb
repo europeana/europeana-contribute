23e5d3dc9f43974283b0a54477c4369453107a5d
Richard Doe <richard.doe@rwdit.net>
2018-01-15 15:14:58 +0100

    Define user role permissions
    - Admin users may do anything
    - Events users may do nothing (for now)

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # Guest users have no privileged access
+    return unless user.present?
+
+    case user.role
+    when :admin
+      can :manage, :all
+    when :events
+      # can what?
+    end
+  end
+end
