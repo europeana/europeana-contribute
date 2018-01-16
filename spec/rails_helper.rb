d871dcc5050c4528fb23d664e61057ebb61e8829
Lutz Biedinger <lutz.biedinger@gmail.com>
2018-01-16 12:38:55 +0100

    move capybara helper before rspec/rails

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,10 +6,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'capybara_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-require 'capybara_helper'
 require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
