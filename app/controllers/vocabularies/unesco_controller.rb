ea14ba7f0901c0d6f8dbc1f1a1bdbffdb5362010
Richard Doe <richard.doe@rwdit.net>
2018-02-02 12:19:38 +0000

    Dereference vocabulary URIs

--- app/controllers/vocabularies/unesco_controller.rb
+++ app/controllers/vocabularies/unesco_controller.rb
@@ -4,14 +4,30 @@ module Vocabularies
   class UNESCOController < VocabulariesController
     vocabulary_index url: 'http://vocabularies.unesco.org/browser/rest/v1/thesaurus/search',
                      params: {
-                       format: 'application/ld+json', lang: 'en', maxhits: 10
+                       format: 'application/ld+json', lang: 'en', maxhits: 10, type: 'skos:Concept'
                      },
                      query: :query,
                      results: 'results',
                      text: :index_result_text,
                      value: 'uri'
 
+#http://vocabularies.unesco.org/browser/rest/v1/thesaurus/data?uri=http://vocabularies.unesco.org/thesaurus/concept10596&format=application/json
+      vocabulary_show url: 'http://vocabularies.unesco.org/browser/rest/v1/thesaurus/data',
+                      params: {
+                        format: 'application/json',
+                        uri: ->(uri) { uri.to_s }
+                      },
+                      text: :show_text
+
     protected
+
+    def show_text(response)
+      item_data = response['graph'].detect { |item| item['uri'] == params['uri'] }
+      translation = item_data['prefLabel'].detect { |pl| pl['lang'] == I18n.locale.to_s } ||
+        item_data['prefLabel'].detect { |pl| pl['lang'] == I18n.default_locale.to_s } ||
+        item_data['prefLabel'].first
+      translation['value']
+    end
 
     def index_query
       "#{params[:q]}*"
