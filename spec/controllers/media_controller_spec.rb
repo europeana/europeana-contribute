2f499d3fb5c1091844f6bc3442c42f73c9cc0618
Richard Doe <richard.doe@rwdit.net>
2018-02-27 14:15:26 +0000

    Media redirects for thumbnails

--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -4,9 +4,14 @@ require 'support/shared_examples/controllers/http_response_statuses'
 
 RSpec.describe MediaController do
   describe 'GET show' do
-    let(:action) { proc { get :show, params: { uuid: uuid } } }
+    let(:action) { proc { get :show, params: params } }
+    let(:params) { { uuid: uuid } }
+    let(:web_resource) do
+      create(:edm_web_resource).tap do |web_resource|
+        web_resource.media.recreate_versions!(:thumb_400x400, :thumb_200x200)
+      end
+    end
 
-    let(:web_resource) { create(:edm_web_resource) }
     let(:uuid) { web_resource.uuid }
 
     context 'when unauthorised' do
@@ -21,6 +26,18 @@ RSpec.describe MediaController do
       context 'when web resource with UUID exists' do
         let(:location) { web_resource.media_url }
         it_behaves_like 'HTTP 303 status'
+
+        context 'with size=w200' do
+          let(:params) { { uuid: uuid, size: 'w200' } }
+          let(:location) { web_resource.media.url(:thumb_200x200) }
+          it_behaves_like 'HTTP 303 status'
+        end
+
+        context 'with size=w400' do
+          let(:params) { { uuid: uuid, size: 'w400' } }
+          let(:location) { web_resource.media.url(:thumb_400x400) }
+          it_behaves_like 'HTTP 303 status'
+        end
       end
 
       context 'when web resource with UUID does not exist' do
--- spec/routing/media_spec.rb
+++ spec/routing/media_spec.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe 'routes for /media' do
+  let(:uuid) { SecureRandom.uuid }
   it 'routes GET /media/:uuid to MediaController#show' do
-    uuid = SecureRandom.uuid
     expect(get("/media/#{uuid}")).to route_to('media#show', uuid: uuid)
+  end
+
+  it 'routes GET /media/:uuid/w200 to MediaController#show' do
+    expect(get("/media/#{uuid}/w200")).to route_to('media#show', uuid: uuid, size: 'w200')
+  end
+
+  it 'routes GET /media/:uuid/w400 to MediaController#show' do
+    expect(get("/media/#{uuid}/w400")).to route_to('media#show', uuid: uuid, size: 'w400')
+  end
+
+  it 'does not route GET /media/:uuid/w100 to MediaController#show' do
+    expect(get("/media/#{uuid}/w100")).not_to route_to('media#show', uuid: uuid, size: 'w100')
   end
 end
