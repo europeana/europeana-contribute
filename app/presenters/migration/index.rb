4ce8a2c93f19b92a7635f230fad5458327b46dd1
andyjmaclean <andyjmaclean@gmail.com>
2018-03-06 14:13:53 +0100

    EC-2711 remove grid parameter from portal search url

--- app/presenters/migration/index.rb
+++ app/presenters/migration/index.rb
@@ -26,7 +26,7 @@ module Migration
           previews: [
             preview_data(1, url: 'https://www.europeana.eu/portal/record/2022608/FBIB_FBib_07004_073.html'),
             preview_data(2, url: 'https://www.europeana.eu/portal/record/2021609/objecten_60411_A_B.html'),
-            preview_data(3, is_person: true, button_text: t('preview_3.button_text'), url: 'https://www.europeana.eu/portal/collections/migration?q=&view=grid'),
+            preview_data(3, is_person: true, button_text: t('preview_3.button_text'), url: 'https://www.europeana.eu/portal/collections/migration?q='),
             preview_data(4, button_opens_form: false, button_text: t('preview_4.button_text'), url: 'https://www.facebook.com/events/1762368494071870/')
           ]
         }
