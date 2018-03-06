e3a7ec1b67339a562f552b0085ec8adac290db83
Richard Doe <richard.doe@rwdit.net>
2018-03-06 23:54:52 +0000

    Document ArrayOfAttributeValidation

--- app/models/concerns/array_of_attribute_validation.rb
+++ app/models/concerns/array_of_attribute_validation.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
+# Validation of attribute values for Mongoid fields typed as `ArrayOf` classes.
+#
+# Any `Mongoid::Document`-including class using `ArrayOf` module classes for
+# its field types should also include this module concern to ensure that
+# non-array values are not permitted to be assigned to those fields.
 module ArrayOfAttributeValidation
   private
 
+  # Overrides `Mongoid::Attributes#validate_attribute_value`
+  # @param (see Mongoid::Attributes#validate_attribute_value)
   def validate_attribute_value(access, value)
     super
     return unless fields[access] && value
