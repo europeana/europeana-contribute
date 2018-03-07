f299687e7f3ff3c1deebcf7a5ec579afef61649c
Richard Doe <richard.doe@rwdit.net>
2018-03-07 09:42:40 +0000

    Spec and document PresenceOfAnyElementValidator

--- app/validators/presence_of_any_element_validator.rb
+++ app/validators/presence_of_any_element_validator.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
-##
-# Presence validator rejecting blank values within an array
+# Presence validator for any element of an array
 #
-# Because `[''].blank? #=> false`
+# Triggers +ActiveModel+'s +:presence+ validation on the array having rejected
+# blank values. Because +[''].blank? #=> false+
+#
+# @example
+#   class Book
+#     include ActiveModel::Model
+#     attr_accessor :authors
+#     validates :authors, presence_of_any_element: true
+#   end
+#
+#   book = Book.new(authors: ['Anna Bell', ''])
+#   book.valid? #=> true
+#
+#   book = Book.new(authors: ['', ''])
+#   book.valid? #=> false
 class PresenceOfAnyElementValidator < ActiveModel::Validations::PresenceValidator
   def validate_each(record, attribute, value)
     return super unless value.is_a?(Array)
