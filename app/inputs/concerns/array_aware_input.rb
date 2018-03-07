71a441eeab33136eabe8b20f4d7e4fe42e364d0b
Richard Doe <richard.doe@rwdit.net>
2018-03-07 10:16:06 +0000

    Document ArrayAwareInput

--- app/inputs/concerns/array_aware_input.rb
+++ app/inputs/concerns/array_aware_input.rb
@@ -1,14 +1,24 @@
 # frozen_string_literal: true
 
+# Mixin to make +SimpleForm+ inputs aware of arrays of attributes
 module ArrayAwareInput
   extend ActiveSupport::Concern
 
+  # Calls +super+ to generate the form input for each value of the array.
+  #
+  # @param wrapper_options [Hash]
+  # @return [String] HTML for the label
   def input(wrapper_options = {})
     array_of_inputs(wrapper_options) do |_index, element_wrapper_options|
       super(element_wrapper_options)
     end
   end
 
+  # Associates the label with the first of the multiple input fields for the array.
+  #
+  # @param wrapper_options [Hash]
+  # @return [String] HTML for the label
+  # @see SimpleForm::Components::Labels#label which this overrides
   def label(wrapper_options = {})
     sup = super
     value = wrapper_options[:value] || @builder.object.send(attribute_name)
@@ -16,6 +26,21 @@ module ArrayAwareInput
     sup.sub(/ for="([^"]*)"/, %( for="\\1_0")).html_safe
   end
 
+  # Generates inputs for all elements of an array.
+  #
+  # Yields each value in the array to a block which should generate the input
+  # HTML for one that one element, then processes the HTML to make it compatible
+  # with Rails handling of arrays from forms.
+  #
+  # The inputs will each be wrapper in an +<li>+ element, all gathered into a
+  # +<ul>+.
+  #
+  # @param wrapper_options [Hash]
+  # @return [String] HTML for the array of inputs for this field
+  # @yieldparam index [Integer,NilClass] index of the element to render an input for
+  # @yieldparam element_wrapper_options [Hash] wrapper options for the element,
+  #   including +:value+
+  # @yieldreturn [String] rendered input for one element (without array awareness)
   def array_of_inputs(wrapper_options = {})
     value = wrapper_options[:value] || @builder.object.send(attribute_name)
 
@@ -23,12 +48,12 @@ module ArrayAwareInput
       return yield nil, wrapper_options
     end
 
-    value.push('') if value.blank?
+    value.push('') if value.blank? # otherwise, no input
     element_fields = value.each_with_index.map do |val, index|
       element_wrapper_options = wrapper_options.merge(value: val)
       element_input = yield index, element_wrapper_options
-      element_input.gsub!(/ name="([^"]*)"/, %( name="\\1[]"))
-      element_input.gsub!(/ (id|data-for)="([^"]*)"/, %( \\1="\\2_#{index}"))
+      element_input.gsub!(/ name="([^"]*)"/, %( name="\\1[]")) # append "[]" to name attr
+      element_input.gsub!(/ (id|data-for)="([^"]*)"/, %( \\1="\\2_#{index}")) # append "_n" where n is element index
       @builder.template.content_tag(:li, element_input.html_safe, class: 'input array-element')
     end
 
