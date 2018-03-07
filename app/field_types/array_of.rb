5d12ac1a95b4bdaf7a7345f2150718e112fc4a0e
Richard Doe <richard.doe@rwdit.net>
2018-03-07 08:43:18 +0000

    Document and spec ArrayOf

--- app/field_types/array_of.rb
+++ app/field_types/array_of.rb
@@ -1,7 +1,50 @@
 # frozen_string_literal: true
 
+# Dynamic creation of classes like +ArrayOf::Date+ used to typecast the elements
+# of array fields when assigning them to Mongoid fields.
+#
+# Classes created by this module sub-class +Array+ and implement +#mongoize+ to
+# convert each array element into the required type.
+#
+# These classes offer no further typecasting of elements than during mongoization.
+#
+# *NB:* Demongoizing does not initialize an instance of the +ArrayOf::+ class.
+# This is intentional behaviour, because +Mongoid::Document+ attribute getters
+# always demongoize, so creating new objects makes the document attribute immutable
+# by methods called on the return value of the getter, e.g. +#push+.
+#
+# @example Typing a Mongoid field with class factory
+#   class Event
+#     include Mongoid::Document
+#     field :dates, type: ArrayOf.type(Date)
+#   end
+#
+# @example Typing a Mongoid field with pre-constructed class
+#   ArrayOf.type(Date) #=> ArrayOf::Date
+#   class Event
+#     include Mongoid::Document
+#     field :dates, type: ArrayOf::Date
+#   end
+#
+# @example Mongoizing a typed field
+#   event = Event.new
+#   event.dates = ['2018-01-01', '2018-01-02']
+#   event.dates #=> [2018-01-01 00:00:00 UTC, 2018-01-02 00:00:00 UTC]
+#   event.dates.class #=> Array
+#
+# @see ArrayOfAttributeValidation
 module ArrayOf
   class << self
+    # +ArrayOf+ class factory
+    #
+    # @param klass [Class] Class to mongoize array elements to
+    # @return [Class] Sub-class of +Array+ in +ArrayOf+ module
+    # @raise [ArgumentError] if the +klass+ param is not a class
+    #
+    # @example Creation of an +Array+ sub-class
+    #   klass = ArrayOf.type(DateTime) #=> ArrayOf::DateTime
+    #   array_of_date_time = klass.new
+    #   array_of_date_time.class #=> ArrayOf::DateTime
     def type(klass)
       fail ArgumentError, "Expected a Class, got a #{klass}" unless klass.is_a?(Class)
       types[klass] ||= begin
@@ -10,6 +53,15 @@ module ArrayOf
       end
     end
 
+    # Registry of classes created by +.type+
+    #
+    # @return [Hash<Class, Class>] keys are the array element class, values are
+    #   the generated +Array+ sub-class
+    #
+    # @example
+    #   ArrayOf.types #=> {}
+    #   ArrayOf.type(Float)
+    #   ArrayOf.types #=> { Float => ArrayOf::Float }
     def types
       @types ||= {}
     end
--- app/models/concerns/array_of_attribute_validation.rb
+++ app/models/concerns/array_of_attribute_validation.rb
@@ -1,21 +1,36 @@
 # frozen_string_literal: true
 
-# Validation of attribute values for Mongoid fields typed as `ArrayOf` classes.
+# Validation of attribute values for Mongoid fields typed as +ArrayOf+ classes.
 #
-# Any `Mongoid::Document`-including class using `ArrayOf` module classes for
+# Any +Mongoid::Document+-including class using +ArrayOf+ module classes for
 # its field types should also include this module concern to ensure that
 # non-array values are not permitted to be assigned to those fields.
+#
+# @example Failure when assigning scalar values
+#   class Event
+#     include Mongoid::Document
+#     include ArrayOfAttributeValidation
+#     field :dates, type: ArrayOf.type(Date)
+#   end
+#
+#   Event.new(dates: '2018-01-13')
+#
+#   # Mongoid::Errors::InvalidValue:
+#   # message:
+#   #   Value of type String cannot be written to a field of type ArrayOf::Date
+#
+# @see ArrayOf
 module ArrayOfAttributeValidation
   private
 
-  # Overrides `Mongoid::Attributes#validate_attribute_value`
+  # Overrides +Mongoid::Attributes#validate_attribute_value+
   # @param (see Mongoid::Attributes#validate_attribute_value)
   def validate_attribute_value(access, value)
     super
     return unless fields[access] && value
     return unless ArrayOf.types.values.include?(fields[access].type)
     unless value.is_a?(Array)
-      raise Mongoid::Errors::InvalidValue.new(Array, value.class)
+      raise Mongoid::Errors::InvalidValue.new(fields[access].type, value.class)
     end
   end
 end
