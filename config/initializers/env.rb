c83ecb11b69d2e725f294ddf05adfdffa46a7ce5
Richard Doe <richard.doe@rwdit.net>
2018-05-04 15:02:43 +0100

    Configurable redirect location for Migration contributions

--- config/initializers/env.rb
+++ config/initializers/env.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+Rails.application.config.x.campaigns = OpenStruct.new(
+  migration: OpenStruct.new(
+    submission_redirect: ENV['CAMPAIGNS_MIGRATION_SUBMISSION_REDIRECT']
+  )
+)
+
 Rails.application.config.x.europeana = OpenStruct.new(
   entities: OpenStruct.new(
     api_url: ENV['EUROPEANA_ENTITIES_API_URL'] || 'https://www.europeana.eu/api'
