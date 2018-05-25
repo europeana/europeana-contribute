6cfbbd4b0e61fd8e747d27a3a39c5e897622d543
Lutz Biedinger <lutz.biedinger@gmail.com>
2018-05-25 10:34:05 +0200

    more specific regex for 'value' detection

--- app/inputs/concerns/array_aware_input.rb
+++ app/inputs/concerns/array_aware_input.rb
@@ -65,7 +65,7 @@ module ArrayAwareInput
   end
 
   def include_field_template(element_fields, array_options)
-    element_field_template = CGI.escapeHTML(element_fields.first.gsub(%(_0"), %(_[[index]]")).gsub(/value=".*?"/, ' value=""'))
+    element_field_template = CGI.escapeHTML(element_fields.first.gsub(%(_0"), %(_[[index]]")).gsub(/ value="[^"]+"/, ' value=""'))
     array_options['data-array-field-template'] = element_field_template
     array_options
   end
