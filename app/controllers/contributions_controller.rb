8f9c017dd09b57f77bfba275de79ee016ccdc200
Lutz Biedinger <lutz.biedinger@gmail.com>
2019-07-23 16:53:35 +0200

    style - index has media logic

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -110,7 +110,7 @@ class ContributionsController < ApplicationController
                     pluck(*Index::Contribution.members).map { |values| Index::Contribution.new(*values) }
     web_resource_ids = aggregations.map(&:edm_isShownBy_id) + aggregations.map(&:edm_hasView_ids).flatten.compact
     web_resources = EDM::WebResource.where('_id': { '$in': web_resource_ids }, 'media': { '$exists': true, '$ne': nil }).
-                      pluck(*Index::EDM::WebResource.members).map { |values| Index::EDM::WebResource.new(*values) }
+                    pluck(*Index::EDM::WebResource.members).map { |values| Index::EDM::WebResource.new(*values) }
     media_aggregation_ids = aggregations.select do |aggregation|
       web_resources.map(&:id).include?(aggregation.edm_isShownBy_id) ||
         !(web_resources.map(&:id) & (aggregation.edm_hasView_ids || [])).empty?
