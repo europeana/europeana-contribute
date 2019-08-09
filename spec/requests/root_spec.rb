fd59399587e1afe76a37f42b719695453e0a39f6
Richard Doe <richard.doe@rwdit.net>
2019-08-09 15:01:05 +0100

    Update / request spec

--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
+require 'support/shared_contexts/stubbed_requests/contentful'
+
 RSpec.describe 'requests for /' do
-  it 'redirects GET / to /migration' do
+  include_context 'Contentful stubbed requests'
+
+  it 'renders /pages/show' do
     get('/')
-    expect(response).to redirect_to('/migration')
+    expect(response).to render_template('pages/show')
   end
 end
