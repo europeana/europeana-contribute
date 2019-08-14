6e7d419a4fb5ea934dbb2af2e57ef0079dd63f01
Richard Doe <richard.doe@rwdit.net>
2019-08-14 10:44:04 +0100

    Update specs

--- spec/requests/migration_spec.rb
+++ spec/requests/migration_spec.rb
@@ -5,11 +5,6 @@ require 'support/shared_contexts/campaigns/migration'
 RSpec.describe 'requests for /migration' do
   include_context 'migration campaign'
 
-  it 'redirects GET / to /migration' do
-    get('/migration')
-    expect(response).to render_template(:index)
-  end
-
   it 'renders template for /migration/new' do
     get('/migration/new')
     expect(response).to render_template(:new)
